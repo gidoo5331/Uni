6881fbdda675f94937c7df8a79fa7e30d7d01a58
Gideon Bempong <gideonbempong533@gmail.com>
2023-04-23 19:12:15 -0900

    added images

--- components/Card/index.js
+++ components/Card/index.js
@@ -7,12 +7,12 @@ const Card = ({ key, name, description, image, price }) => {
       <div className="w-40 lg:w-52 h-full text-center mt-10">
         <div className="relative">
           <Image src={image} width={232} height={232} alt="img" className=" w-[232.47px] h-[232.47px] object-cover" />
-          <button
+          {/* <button
             type="text"
             className="absolute -bottom-3 left-12 lg:left-20 bg-red text-sm text-white rounded px-3"
           >
             &#163; {price}
-          </button>
+          </button> */}
         </div>
         <div className="pt-4">
           <p className="text-lg text-red">{name}</p>
