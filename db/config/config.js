eca64ef9ee884c5c958af5c987ff0c9312d5ed7e
Gideon Bempong <gideonbempong533@gmail.com>
2023-04-26 12:23:09 -0900

    migrated db

--- db/config/config.js
+++ db/config/config.js
@@ -1,10 +1,16 @@
 module.exports = {
   development: {
-    dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD ,
-    database:process.env.DB_DATABASE ,
-    host:process.env.DB_HOST ,
+    "dialect": process.env.DB_DIALECT,
+    "username": process.env.DB_USERNAME,
+    "password":process.env.DB_PASSWORD ,
+    "database":process.env.DB_DATABASE ,
+    "host":process.env.DB_HOST,
+    "ssl": true,
+    "dialectOptions": {
+      "ssl": {
+        "require": true
+      }
+    }
   },
   test: {
     dialect: process.env.DB_DIALECT,
