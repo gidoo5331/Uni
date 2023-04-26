9b7b7fc905ac614a9a1fea06166c8f475de67d90
Gideon Bempong <gideonbempong533@gmail.com>
2023-04-26 12:30:34 -0900

    edited

--- db/config/config.js
+++ db/config/config.js
@@ -1,31 +1,24 @@
 module.exports = {
-  development: {
+  "development": {
     "dialect": process.env.DB_DIALECT,
     "username": process.env.DB_USERNAME,
     "password":process.env.DB_PASSWORD ,
     "database":process.env.DB_DATABASE ,
     "host":process.env.DB_HOST,
+  },
+  "test": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres",
+  },
+  "production": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres",
     "ssl": true,
-    "dialectOptions": {
-      "ssl": {
-        "require": true
-      }
-    }
-  },
-  test: {
-    dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_DEVELOPMENT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  },
-  production: {
-    dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_DEVELOPMENT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  },
+  }
 }
